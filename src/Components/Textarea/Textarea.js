fd1c2484b6795f9db24a0865b7e37ffa9369e4b9
Damien MIREMONT <damien.miremont@gmail.com>
2022-07-24 11:49:01 +0200

    initialisation du projet

---
+++ src/Components/Textarea/Textarea.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Textarea.css";
+
+function TextArea() {
+    return (
+        <div className='textarea'>
+            <h1>Ceci est un test !</h1>
+        </div>
+
+    );
+}
+
+export default TextArea;
---
+++ src/Containers/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./App.css";
+
+import TextArea from '../../Components/Textarea/Textarea';
+
+function App() {
+  return (
+    <div className="App">
+      <article className='main_container'>
+        
+      <TextArea />
+      <TextArea />
+      </article>
+    </div>
+  );
+}
+
+export default App;
